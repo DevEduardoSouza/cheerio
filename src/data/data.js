08589bb4dcc3487b5f8a2c597d55b2af32d3aa12
DevEduardoSouza <eduardosouza.din2@gmail.com>
2024-02-05 22:22:11 -0300

    feat: add data

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,19 @@
-const data = { nome: "Opa" };
+const data = [
+  {
+    Ecommerces: [
+      {
+        name: "mercadolivre",
+        link: "mercadolivre.com.br",
+        termOfSearch: "",
+        sectores: {
+          products: "",
+          prices: "",
+          description: "",
+          img: "",
+        },
+      },
+    ],
+  },
+];
 
 export default data;
